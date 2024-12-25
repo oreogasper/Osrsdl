3c91a53c153804ba4d75960929906c7f937ab36f
oreogasper <1chowemm@gmail.com>
2024-12-25 03:21:46 -0500

    new testing

--- classic/saveOperators.php
+++ classic/saveOperators.php
@@ -2,5 +2,12 @@
 // Get the JSON string from the request body
 $jsonString = file_get_contents('php://input');
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo 'Method Not Allowed';
+    exit;
+}
+
 // Write the JSON string back to the boss.json file
 file_put_contents('boss.json', $jsonString);
+file_put_contents('debug.log', json_encode($_POST, JSON_PRETTY_PRINT));
